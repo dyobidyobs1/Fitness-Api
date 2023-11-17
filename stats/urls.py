4fdafb14f80763125600bb0278cdee1a3cc43bc4
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2023-11-18 02:19:10 +0800

    update

---
+++ stats/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path, path
+from . import views
+
+
+
+urlpatterns = [
+    re_path('login', views.login),
+    re_path('register', views.register),
+    re_path('test_token', views.test_token),
+    ]
